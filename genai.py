82c4820f6fa8f082156787f6f064f38daa84901b
Dvcken <demid2885@mail.com>
2024-10-30 15:47:37 +0300

    Temp change for corpus added classes for AI

--- genai.py
+++ genai.py
@@ -1,24 +1,31 @@
 import google.generativeai
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
+from handlerofmessages import LanguageModels
 
 #Module for Google Gemini
+class GenAI(LanguageModels):
+    def __init__(self, prompt):
+        super().__init__(prompt=prompt)
 
-GEMINI_TOKEN: str = os.getenv('GEMINI_TOKEN')
+    GEMINI_TOKEN: str = os.getenv('GEMINI_TOKEN')
 
-google.generativeai.configure(api_key=GEMINI_TOKEN)
+    google.generativeai.configure(api_key=GEMINI_TOKEN)
 
-def generate_genai_response(message: str) -> str:
-    try:
-        model = google.generativeai.GenerativeModel('gemini-1.5-flash')
-        response = model.generate_content(message)
-        return response.text
-    except Exception as e:
-        return "Gemini Ai hasn't processed message"
+    def generate_response(self, prompt: str) -> str:
+        try:
+            model = google.generativeai.GenerativeModel('gemini-1.5-flash')
+            response = model.generate_content(prompt)
+            return response.text
+        except Exception as e:
+            return "Gemini Ai hasn't processed message"
 
-#DEBUG, please, do not launch this script!
+
+
+
+# DEBUG, please, do not launch this script!
 if __name__ == '__main__':
     message1: str = 'difference between helicopter and plane'
-    generate_genai_response(message1)
+    GenAI.generate_response(message1)
+
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from telegram import Update
 from telegram.ext import *
-from handler_of_messages import *
+from handlerofmessages import *
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TELEGRAM_TOKEN: str = os.getenv('TELEGRAM_TOKEN')
-print(TELEGRAM_TOKEN)
+
 
 def main() -> None:
     #Bot initialization
     app = Application.builder().token(TELEGRAM_TOKEN).build()
 
     #Detects telegram._Update and sends it to corresponding method
-    app.add_handler(CommandHandler('start', Handler_of_messages.start_command, has_args=False))
-    app.add_handler(CommandHandler('help', Handler_of_messages.help_command, has_args=False))
-    app.add_handler(CommandHandler('mode', Handler_of_messages.mode_command))
-    app.add_handler(MessageHandler(filters.TEXT, Handler_of_messages.message_response))
+    app.add_handler(CommandHandler('start', HandlerOfMessages.start_command, has_args=False))
+    app.add_handler(CommandHandler('help', HandlerOfMessages.help_command, has_args=False))
+    app.add_handler(CommandHandler('mode', HandlerOfMessages.mode_command))
+    app.add_handler(MessageHandler(filters.TEXT, HandlerOfMessages.message_response))
 
     #Bot starts to see messages
+    print('Start polling...')
     app.run_polling()
 
 
